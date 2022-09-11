ba36324643a35e0a03d883d1164c72eef95b3b06
Divyansh Pratap Singh <104272009+dev-divyansh@users.noreply.github.com>
2022-09-11 23:42:47 +0530

    Initial commit

---
+++ app/src/main/java/com/divyansh/buddylocator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.divyansh.buddylocator;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        if(!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            int REQUEST_ENABLE_BT = 1;
+            startActivityForResult(enableBtIntent , REQUEST_ENABLE_BT);
+        }
+
+
+        
+
+
+
+
+    }
+    protected void proceed(){
+
+    }
+}
